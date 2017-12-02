4546c577e6300cd1c52d83219237c7a07bcc969d
Coulby Nguyen <nguyenco@oregonstate.edu>
2017-12-02 12:50:12 -0800

    created the index.js file and prototyped the function

---
+++ public/index.js
@@ -0,0 +1,11 @@
+// show modal on add click
+
+// hide modal on exit click
+
+// Clear out modal fields
+
+//insert new post function
+
+//parse fields function
+
+//wait until all dom elements have been loaded in
