a25ec434648ae9700907e4f070b6685a8e1bc371
SondiDK <olle_s_larsen@hotmail.com>
2017-12-04 22:06:12 -0800

    it now works like assignment 5 loading from json

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
-app.use(express.static('public'));
 
+
+var postData = require('./postData');
+
+app.get('/', function (req, res, next) {
+
+res.status(200).render('postPage', {
+postItems: postData
+
+});
+
+});
+
+app.use(express.static('public'));
 
 app.listen(port, function () {
   console.log("== Server listening on port", port);
