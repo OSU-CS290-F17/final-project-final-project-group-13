adb9a937edba019e5951572153e244499f8968a3
SondiDK <olle_s_larsen@hotmail.com>
2017-12-05 15:05:42 -0800

    final updates

--- public/postTemplate.js
+++ public/postTemplate.js
@@ -15,7 +15,7 @@ templates['post'] = template({"compiler":[7,">= 4.0.0"],"main":function(containe
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + "\">\n    </div>\n    <div class=\"eventDetails\">\n      <a href=\"#\" class=\"eventTitle\"> "
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + " </a> <span class=\"date\">"
+    + " </a><br> <span class=\"date\">"
     + alias4(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data}) : helper)))
     + "</span> <span class=\"time\">"
     + alias4(((helper = (helper = helpers.time || (depth0 != null ? depth0.time : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"time","hash":{},"data":data}) : helper)))
